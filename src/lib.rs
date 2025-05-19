c793ac703961b310531f912ea0538072940bb9e2
Christopher Sardegna <github@reagentx.net>
2025-05-18 18:48:51 -0700

    remove unnecessary re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,5 @@ pub mod error;
 /// Main library for interacting with iOS backups, providing types and methods for decryption and metadata access.
 pub use backup::Backup;
 
-/// File entry representation in the backup database, including metadata and encryption info.
-pub use crate::backup::models::file::BackupFileEntry;
-
 /// Authentication options for encrypted backups: either a password or a pre-derived key.
 pub use backup::models::auth::Authentication;
-
-/// Retrieve basic device metadata from a given backup folder UDID.
-pub use backup::device::get_device_basic_info;
-
-/// The error type for all operations in the `crabapple` crate.
-pub use error::{BackupError, Result};
